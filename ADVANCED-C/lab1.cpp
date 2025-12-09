8985e7889fa2be35b4a63f92796e68194a4f9e45
MlnwZa007 <684244148@mail.pbru.ac.th>
2025-12-09 16:05:14 +0700

    Add files via upload

---
+++ ADVANCED-C/lab1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace  std;
+
+int power(int base, int exponent) {
+    if (exponent == 0)
+        return 1;
+    else
+        return base * power(base, exponent -1);  
+} 
+
+int main(){
+    int base = 2, exponent = 10;
+
+    int result = power(base, exponent);
+    cout << base << " ^ " << exponent << " is " << power(base, exponent) << endl;
+    return 0; 
+}
